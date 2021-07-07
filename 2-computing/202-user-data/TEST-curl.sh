70a3ba1b42b95188e7176ee15be7ddf6abcc7f0c
guillaumem-theodo <guillaumem@theodo.fr>
2021-07-07 23:00:09 +0200

    cpu-202-user-data

---
+++ 2-computing/202-user-data/TEST-curl.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+ec2_1_public_ip=$(aws ec2 describe-instances --region "$TUTORIAL_REGION"  --profile aws-workout --filters Name=tag:Name,Values="cpu-202-ec2-1"  Name=instance-state-code,Values=16 --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+echo "✅ Trying to CURL site with public IP: $ec2_1_public_ip"
+curl "http://$ec2_1_public_ip:80"
