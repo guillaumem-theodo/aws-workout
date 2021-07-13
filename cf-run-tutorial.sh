ed97c3e0e9e3f7f5677295cf704f35ce0f86f48f
guillaumem-theodo <guillaumem@theodo.fr>
2021-07-13 18:10:04 +0200

    Fix a bug in CloudFormation dependencies mgt

--- cf-run-tutorial.sh
+++ cf-run-tutorial.sh
@@ -9,13 +9,20 @@ if [ ! -d "$1"/cf ]; then
   echo "Tutorial Path does not contain 'cf' subdir" && exit 1;
 fi
 
+
+TUTORIAL_DIR=$1
+TUTORIAL_KEY="stack-$(basename "$TUTORIAL_DIR")"
+
+source ./backend-env.conf
+
 ## CHECK DEPENDENCIES
 if [ -f "$1"/dep.txt ]; then
   cat "$1"/dep.txt | while read line
   do
    # Check if dependency has been properly applied
    # Check if the cloudformation stack of the dependency has been applied successfully
-    found=$(aws cloudformation list-stacks --region eu-west-1 --profile aws-workout --query "StackSummaries[?StackName == 'stack-$line'].StackName" --stack-status-filter CREATE_COMPLETE --output text)
+    stackname=$(basename "$line")
+    found=$(aws cloudformation list-stacks --region "$TUTORIAL_REGION" --profile aws-workout --query "StackSummaries[?StackName == 'stack-$stackname'].StackName" --stack-status-filter CREATE_COMPLETE --output text)
     if [ -z "$found" ]; then
       echo "❌ Tutorial '$1' requires the tutorial >>>'$line'<<< to be applied first !!!" && exit 1;
     fi
@@ -24,11 +31,6 @@ if [ -f "$1"/dep.txt ]; then
   echo "✅ All dependencies have been properly found !!!"
 fi
 
-
-TUTORIAL_DIR=$1
-TUTORIAL_KEY="stack-$(basename "$TUTORIAL_DIR")"
-
-source ./backend-env.conf
 echo "Applying CloudFormation Tutorial '$TUTORIAL_KEY' in $TUTORIAL_REGION"
 (cd "$1"/cf || exit; aws cloudformation create-stack --stack-name "$TUTORIAL_KEY" --region "$TUTORIAL_REGION" --profile aws-workout --template-body file://./stack.yaml)
 
