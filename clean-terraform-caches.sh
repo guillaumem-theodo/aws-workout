ff1a14c83e12c0cca409655e57dd17a9f5453b75
guillaumem-theodo <guillaumem@theodo.fr>
2022-09-22 18:35:18 +0200

    add a clean terraform cache command

--- clean-terraform-caches.sh
+++ clean-terraform-caches.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env bash
-source ./backend-env.conf
-aws resourcegroupstaggingapi get-resources --profile aws-workout --region $TUTORIAL_REGION --output text --tag-filters "Key=Purpose,Values=aws-workout"
-aws resourcegroupstaggingapi get-resources --profile aws-workout --region $TUTORIAL_ANOTHER_REGION --output text --tag-filters "Key=Purpose,Values=aws-workout"
+find . -type d -name ".terragrunt-cache" -prune -exec rm -rf {} \;
